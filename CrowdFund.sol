e95e90c3873bce1d84d2683b97eb093dd05fc0b2
Uttam Sah <uttamkumarsah111@gmail.com>
2024-03-27 01:35:42 +0530

    push

---
+++ CrowdFund.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract CrowdFunding {
+    address public owner; // owner of the contract
+    uint256 public minimumContribution; // minimum contribution required for contributor to do crowd funding.
+    uint256 public deadline; // Final Time to do crowdfunding.
+    uint256 public target;
+    uint256 public raisedAmount; // Keep track of the total raised amount.
+    uint256 public totalContributors; // total contributors who have contributed to crowd funding.
+    uint256 public numRequests;
+
+    struct Request {
+        string description;
+        address payable recipient;
+        uint256 value;
+        bool completed;
+        uint256 noOfVoters;
+        mapping(address => bool) voters;
+    }
+
+    mapping(uint256 => Request) public allRequests;
+    mapping(address => uint256) public contributors; // key value pair of contributors address and fund they have contributed.
+
+    constructor(uint256 _target, uint256 _deadline) {
+        owner = msg.sender;
+        deadline = block.timestamp + _deadline;
+        target = _target;
+        minimumContribution = 100 wei;
+    }
+
+    // All Modifiers starts here --------------------------------------------------
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can access this function.");
+        _;
+    }
+
+    modifier isDeadlinePassed() {
+        require(
+            block.timestamp < deadline,
+            "Crowd Funding deadline has passed. Please try again later."
+        );
+        _;
+    }
+
+    modifier isContributor() {
+        require(contributors[msg.sender] > 0, "Sorry, you are not a contributor. Try to contribute to crowd funding then try again. Thanks.");
+        _;
+    }
+
+    // All Modifiers Ends here --------------------------------------------------
+
+    // get balance
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    // Contribute
+    function contribute() public payable isDeadlinePassed {
+        require(
+            msg.value >= minimumContribution,
+            "Minimum 100 wei is required to contribute."
+        );
+
+        if (contributors[msg.sender] == 0) {
+            totalContributors++;
+        }
+
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+    // Refund the money if the target is not fulfilled and deadline has passed.
+    function refund() public isDeadlinePassed isContributor {
+        require(raisedAmount < target, "You are not eligible for refund");
+        require(contributors[msg.sender] > 0);
+
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+    }
+
+    // Create Request
+    function createRequest(
+        string memory _description,
+        address payable _recipient,
+        uint256 _value
+    ) public onlyOwner {
+        Request storage newRequest = allRequests[numRequests];
+        numRequests++;
+
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+
+    // vote request.
+    function voteRequest(uint256 _requestNo) public isContributor {
+        Request storage currentRequest = allRequests[_requestNo];
+        require(currentRequest.voters[msg.sender] == false, "you have already voted");
+        currentRequest.voters[msg.sender] = true;
+        currentRequest.noOfVoters++;
+    }
+
+    // make payemnt only if the voters are greater than half of the contributors.
+    function makePayment(uint _requestNo) public onlyOwner{
+        require(raisedAmount >= target);
+        Request storage currentRequest = allRequests[_requestNo];
+        require(currentRequest.completed == false, "This request has already completed");
+        require(currentRequest.noOfVoters > totalContributors/2, "Contributor does not support. Votes are low.");
+        currentRequest.recipient.transfer(currentRequest.value);
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Altru
+# Crowd Funding Smart Contract in Solidity
+
+
+# Things used in the project :-
+
+1. Solidity programming language.
+2. Ethereum.
+3. Remix IDE.
+
+
+# Architecture of the project :-
+
+**Global Variables** :- 
+1. owner -> owner of the contract
+2. minimumContribution -> minimum contribution required for contributor to do crowd funding.
+3. deadline -> Final Time to do crowdfunding.
+4. target -> crowd funding target (How much funds is targeted to be completed in the deadline);
+5. raisedAmount -> Keep track of the total raised amount.
+6. totalContributors -> total contributors who have contributed to crowd funding.
+7. numRequests -> keep track of requets done by owner.
+8. contributors -> key value pair of contributor to keep track of their address and funds they have contributed.
+9. allRequests -> All requests done by owner for tranfering money to a perticular address for a perticular reason.
+
+
+**Functions**  :- 
+1. getBalance
+2. contribute
+3. refund
+4. createRequest
+5. voteRequest
+6. Make payment
+
+
+
+# How It works :-
+
+1. Initially using constructor the owner addres, deadline, target, and minimum contribution to be done by contributor is saved in state variable.
+
+2. The contributor contributes using contribute() function in crowd funding. If the deadline is not over and the funds are meeting the minimum requirements then the address and value is stored in state variable mapping.
+
+3. If the target is not met and contributor wants the refund then he can refund the money by using refund() function.
+
+4. The owner can request the all contributors to withdraw the funds by uing createRequest() function and tell the description, address to which the funds will be sent, and the total funds which to be sent.  
+
+5. All the contributors vote using voteRequest() function and if the votes are more than the 50% of contributors then using the makePayment() function the money is sent to the perticular address which owner has provided. 
